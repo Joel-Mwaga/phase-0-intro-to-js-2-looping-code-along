c117003f9ba69123eed3e48626d6f05bfc00ea89
Joel Mwaga <mwagahjoel14@gmail.com>
2025-03-02 17:25:31 +0300

    changed 1 file

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+writeCards();
+countDown();
+function countDown(){
+    let count = 10;
+    while(count >= 0){
+        console.log(count);
+        count--;
+    }
+}
+function writeCards(){
+    let names = ["Guadalupe", "Ollie", "Aki"];
+    let event = "surprise";
+    let messages = [];
+    for(let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
